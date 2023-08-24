d6969e42d001402f9ed0fda6812e2b10b7ca8754
Haritha Raghu <haritharaghu1998>
2023-08-24 23:12:00 +0530

    Updated code for Contact feature

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,50 @@
 import React from "react";
+import { useState } from "react";
 import './index.scss'
 
 const Contact =()=>{
 
+      const [formData,setFormData]=useState(
+        {
+            name: '',
+            email: '',
+            message: '',
+
+        }
+      )
+      const handleChange=(e)=>{
+        const {name,value}=e.target;
+           setFormData((prevData)=>({
+                 ...prevData,
+                 [name]:value,
+           })
+          
+          )
+      }
+      const handleSubmit=(e)=>{
+        e.preventDefault(e);
+        const { name, email, message } = formData;
+        console.log('Name:', name);
+        console.log('Email:', email);
+        console.log('Message:', message);
+      }
     return(
      <>
         <div id='contact' className="contact-content">
             
-            <form className="form-content">
+            <form className="form-content" onSubmit={handleSubmit}>
             <h2>CONTACT ME</h2>
                 <div className="form-item">
                  <label htmlFor="name">NAME:</label><br/>
-                 <input type='text' id="name" name="name" required/>
+                 <input type='text' id="name" name="name" value={formData.name} onChange={handleChange} required/>
                  </div>
                  <div className="form-item">
                  <label htmlFor="email">EMAIL:</label><br/>
-                 <input type='email' id="email" name="email" required/>
+                 <input type='email' id="email" name="email" value={formData.email}  onChange={handleChange} required/>
                  </div>
                  <div className="form-item">
                  <label htmlFor="message">MESSAGE:</label><br/>
-                 <textarea id="message" name="message"  rows='4' required/>
+                 <textarea id="message" name="message"  rows='4' value={formData.message}  onChange={handleChange} required/>
                  </div>
                  <button type="submit">SEND</button>
             </form>
@@ -29,4 +54,4 @@ const Contact =()=>{
     )
 }
 
-export default Contact
+export default Contact;
